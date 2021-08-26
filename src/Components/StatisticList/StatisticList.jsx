0921eb8951be66fd540127ee4fc6adcc2f0c549e
PaulPugovkin <coastalrhodes@gmail.com>
2021-08-26 11:58:08 +0300

    Update StatisticList.jsx

--- src/Components/StatisticList/StatisticList.jsx
+++ src/Components/StatisticList/StatisticList.jsx
@@ -2,20 +2,20 @@ import styles from './StatisticList.module.css';
 import StatisticItem from '../StatisticItem/StatisticItem';
 import { v4 as uuidv4 } from 'uuid';
 
-const StatisticList = props => {
-    const optionsKeys = Object.keys(props.options);
+const StatisticList = ({ options, total, positive }) => {
+    const optionKeys = Object.keys(options);
     return (
         <div className={styles.statWrapper}>
             <ul className={styles.statisticList}>
-                {optionsKeys.map(option => (
+                {optionKeys.map(option => (
                     <StatisticItem
                         key={uuidv4()}
                         option={option}
-                        value={props.options[option]}
+                        value={options[option]}
                     />
                 ))}
-                <li>Total: {props.total}</li>
-                <li>Positive percentage: {props.positive}%</li>
+                <li>Total: {total}</li>
+                <li>Positive percentage: {positive}%</li>
             </ul>
         </div>
     );
